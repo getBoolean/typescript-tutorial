05b2e8b34d27bd735171eb3c23d2cc4b25849c1d
getBoolean <danielwnewville@gmail.com>
2021-11-26 12:56:44 -0600

    l12

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ class Invoice {
   details: string;
   amount: number;
 
-  constructor(c: string, d: string, a: number){
+  constructor(c: string, d: string, a: number) {
     this.client = c;
     this.details = d;
     this.amount = a;
@@ -17,6 +17,8 @@ class Invoice {
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+console.log(invOne);
+console.log(invTwo);
 
 let invoices: Invoice[] = [];
 invoices.push(invOne)
